f34c0be1f7083063a8aa0d1b586bbebdbc8d3008
Alison Kohl <akohl@stanford.edu>
2015-02-02 09:56:08 -0800

    creating accounts

--- watchwithus/app.js
+++ watchwithus/app.js
@@ -10,6 +10,7 @@ var myFirebaseRef = new Firebase("https://watchwithus.firebaseio.com/");
 
 var routes = require('./routes/index');
 var login = require('./routes/login');
+var createaccount = require('./routes/createaccount');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/login', login);
+app.use('/createaccount', createaccount);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- watchwithus/routes/login.js
+++ watchwithus/routes/login.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var Firebase = require("firebase");
+var db = new Firebase("https://watchwithus.firebaseio.com/");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Login' });
+});
+
+router.post('/', function(req, res, next) {
+	var form_data = req.body;
+	db.createUser({
+		name: form_data.name,
+		email: form_data.email,
+		password: form_data.password
+	}, function(error) {
+		if (error == null) {
+			console.log("User created successfully");
+			res.render('login', { title: 'Login' });
+		} else {
+			console.log("Error creating user: ", error);
+		}
+	});
 });
 
 module.exports = router;
