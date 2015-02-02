7e18794191b8f9ad224d6eca2e1b8b441b7ab3a9
Palani Eswaran <palani14@stanford.edu>
2015-02-01 21:32:33 -0800

    Initial setup of node.js framework

--- watchwithus/app.js
+++ watchwithus/app.js
@@ -1,21 +1,25 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var Firebase = require("firebase");
+var myFirebaseRef = new Firebase("https://watchwithus.firebaseio.com/");
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon('./public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,3 +62,4 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+app.listen(3000);
