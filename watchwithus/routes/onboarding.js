28db88f151b4a187487da7db9487f8cf26a6eb1f
Palani Eswaran <palani14@stanford.edu>
2015-02-08 16:53:36 -0800

    working on onboarding backend

--- watchwithus/routes/onboarding.js
+++ watchwithus/routes/onboarding.js
@@ -21,23 +21,67 @@ router.post('/', function(req, res, next) {
 		var index = form_data.index;
 		var properIndex = parseInt(index) + 1;
 
-		moviesArray = [];
 
-		request({
-			uri: "http://api.rottentomatoes.com/api/public/v1.0/movies/" + movieIds[properIndex] + ".json?apikey=v67jb7aug6qwa4hnerpfcykp",
-			method: "GET",
-	}, function(error, response, body) {
+		var movieId = "";
 
-			var doc = JSON.parse(body);
-			var title = doc.title;
-			var synopsis = doc.synopsis;
-			var thumbnail = doc.posters.thumbnail;
-			var audience_score = doc.ratings.audience_score;
+		/*var authClient = new FirebaseSimpleLogin(myRef, function(error, user) {
+	  		if (error) {
+	    		// an error occurred while attempting login
+	    		console.log(error);
+	  		} else if (user) {
+	    		// user authenticated with Firebase
+	    		console.log("User ID: " + user.uid + ", Provider: " + user.provider);
+	  		} else {
+	    		// user is logged out
+	  		}
+		});*/
 
-			moviesArray.push({'title': title, 'synopsis': synopsis, 'thumbnail': thumbnail, 'audience_score': audience_score});			
-			
-			res.render('onboarding', {title: 'Onboarding', 'movies': moviesArray, 'index': properIndex});
+		var authData = db.getAuth();
+		console.log("userId: " + authData.uid);
+		uid = authData.uid;
+
+		var usersRef = new Firebase("https://watchwithus.firebaseio.com/users");
+
+		usersRef.orderByKey().equalTo(uid).on("child_added", function(snapshot) {
+			var index = snapshot.val().index;
+			var moviesRated = snapshot.val().moviesRated;
+			console.log("index: " + index);
+			console.log("moviesRated: " + moviesRated);
+
+			var newIndex = parseInt(index) + 1;
+			//if rated
+			var newMoviesRated = parseInt(moviesRated) + 1;
+			//else don't increment it
+
+			var specificUserRef = new Firebase("https://watchwithus.firebaseio.com/users/" + uid);
+			specificUserRef.update({index: newIndex, moviesRated: newMoviesRated});
+
+			var moviesToRateRef = new Firebase("https://watchwithus.firebaseio.com/moviesToRate");
+
+			moviesToRateRef.orderByKey().equalTo(newIndex.toString()).on("child_added", function(snapshot) {
+		  		movieId = snapshot.val();
+		  		console.log("movieId: " + movieId);
+
+				request({
+					uri: "http://api.rottentomatoes.com/api/public/v1.0/movies/" + movieId + ".json?apikey=v67jb7aug6qwa4hnerpfcykp",
+					method: "GET",
+				}, function(error, response, body) {
+					var doc = JSON.parse(body);
+					var title = doc.title;
+					var synopsis = doc.synopsis;
+					var thumbnail = doc.posters.thumbnail.substring(0, doc.posters.thumbnail.length-7) + "det.jpg";
+					var audience_score = doc.ratings.audience_score;
+
+					moviesArray.push({'title': title, 'synopsis': synopsis, 'thumbnail': thumbnail, 'audience_score': audience_score});
+								
+
+					res.render('onboarding', {title: 'Onboarding', 'title': title, 'synopsis': synopsis, 'thumbnail': thumbnail, 'audience_score': audience_score, 'index': properIndex});
+
+				});
+			});
+
 		});
+
 	} else {
 
 		db.createUser({
@@ -73,10 +117,11 @@ router.post('/', function(req, res, next) {
 		  				db2.set({
 		  					name: form_data.name,
 		  					ratings: {},
-		  					index: -1
+		  					index: 0,
+		  					moviesRated: 0
 		  				});
 
-		  				var db2 = db.child("users/user2");
+		  				/*var db2 = db.child("users/user2");
 		  				db2.set({
 		  					name: form_data.name,
 		  					ratings: {},
@@ -93,29 +138,42 @@ router.post('/', function(req, res, next) {
 		  				db4.push({
 		  					mid: 8654,
 		  					rating: 4
-		  				});
+		  				});*/
 					}
 		  		});
 
 		  		moviesArray = [];
 
-		  		request({
-		      		uri: "http://api.rottentomatoes.com/api/public/v1.0/movies/" + movieIds[properIndex] + ".json?apikey=v67jb7aug6qwa4hnerpfcykp",
-		      		method: "GET",
-				}, function(error, response, body) {
-					var doc = JSON.parse(body);
-					var title = doc.title;
-					var synopsis = doc.synopsis;
-					var thumbnail = doc.posters.thumbnail;
-					var audience_score = doc.ratings.audience_score;
+		  		var moviesToRateRef = new Firebase("https://watchwithus.firebaseio.com/moviesToRate");
 
-					moviesArray.push({'title': title, 'synopsis': synopsis, 'thumbnail': thumbnail, 'audience_score': audience_score});
-					
+		  		var movieId = "";
 
-					res.render('onboarding', {title: 'Onboarding', 'movies': moviesArray, 'index': properIndex});
-				
+		  		moviesToRateRef.orderByKey().equalTo("0").on("child_added", function(snapshot) {
+		  			movieId = snapshot.val();
+		  			console.log("movieId: " + movieId);
 
-			});
+				  		request({
+				      		uri: "http://api.rottentomatoes.com/api/public/v1.0/movies/" + movieId + ".json?apikey=v67jb7aug6qwa4hnerpfcykp",
+				      		method: "GET",
+						}, function(error, response, body) {
+							var doc = JSON.parse(body);
+							var title = doc.title;
+							var synopsis = doc.synopsis;
+							var thumbnail = doc.posters.thumbnail.substring(0, doc.posters.thumbnail.length-7) + "det.jpg";
+							var audience_score = doc.ratings.audience_score;
+
+							moviesArray.push({'title': title, 'synopsis': synopsis, 'thumbnail': thumbnail, 'audience_score': audience_score});
+							
+
+					res.render('onboarding', {title: 'Onboarding', 'title': title, 'synopsis': synopsis, 'thumbnail': thumbnail, 'audience_score': audience_score, 'index': properIndex});
+						
+
+						});
+		  		});
+
+
+
+		  		
 			} else {
 				console.log("Error creating user: ", error);
 			}
