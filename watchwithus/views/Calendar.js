e97eb996d96912d20c3e617b36f6ca147abda6e0
Palani Eswaran <palani14@stanford.edu>
2015-02-24 16:41:02 -0800

    removed add friends button from mainmenu page

--- watchwithus/views/Calendar.js
+++
@@ -1,7 +0,0 @@
-function Calendar(divName) {
-	this.element = document.getElementById(divName);
-	this.render = function(date) {
-		var numDaysInMonth = new Date(date.getYear(), date.getMonth() + 1, 0).getDate();
-		var day = new Date(date.getYear(), date.getMonth(), 0).getDay();
-	}
-}
