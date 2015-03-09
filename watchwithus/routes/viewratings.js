697af48178cf317deb11273f747e3034ce2c8de0
Palani Eswaran <palani14@stanford.edu>
2015-03-09 10:36:16 -0700

    created array in viewRatings router. need to sort before passing it to frontend

--- watchwithus/routes/viewratings.js
+++ watchwithus/routes/viewratings.js
@@ -6,8 +6,22 @@ var url = require('url');
 var request = require('request');
 
 router.get('/', function(req, res, next) {
-	//get movie ratings from db here
-	res.render('viewratings');
+	var authData = db.getAuth();
+	uid = authData.uid;
+
+	var specificUserRatingsRef = new Firebase("https://watchwithus.firebaseio.com/users/" + uid);
+
+	specificUserRatingsRef.orderByChild("ratings").on("child_added", function(snapshot) {
+		if (snapshot.key() == "ratings") {
+			var userRatings = Object.keys(snapshot.val()).map(function(k) { return snapshot.val()[k] });
+			var ratingsArr = new Array();
+			for (var i = 0; i < userRatings.length; i++) {
+				ratingsArr.push({'title': userRatings[i].title, 'rating': userRatings[i].rating});
+			}
+			res.render('viewratings', {title: 'View Ratings', 'ratings': ratingsArr});
+		}
+	});
+
 });
 
 module.exports = router;
