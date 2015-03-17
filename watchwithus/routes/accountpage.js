71b3b36c8affc543400832df65453ad2736764b0
Palani Eswaran <palani14@stanford.edu>
2015-03-17 13:21:13 -0700

    accountpage.js fleshed out for changed email

--- watchwithus/routes/accountpage.js
+++ watchwithus/routes/accountpage.js
@@ -5,6 +5,7 @@ var db = new Firebase("https://watchwithus.firebaseio.com/");
 var request = require('request');
 
 router.get('/', function(req, res, next){
+
 	var authData = db.getAuth();
 
 	if (authData == null) {
@@ -30,6 +31,28 @@ router.get('/', function(req, res, next){
 		});
 	}
 
+});
+
+router.post('/', function(req, res, next) {
+
+	var form_data = req.body;
+	var email_changed = form_data.emailChanged;
+	var username = form_data.username;
+	if (email_changed == "true") {
+		db.changeEmail({
+		  oldEmail : form_data.oldEmail,
+		  newEmail : form_data.email,
+		  password : form_data.password
+		}, function(error) {
+		  if (error === null) {
+		    console.log("Email changed successfully");
+		    res.render('account', {title: 'Account Page', 'username': username, 'email': form_data.email, 'message': "Successfully changed email address!" });
+		  } else {
+		    console.log("Error changing email:", error);
+		   	res.render('account', {title: 'Account Page', 'username': username, 'email': form_data.oldEmail, 'message': "Could not change email address. Please try again." });
+		  }
+		});
+	}
 });
 
 
