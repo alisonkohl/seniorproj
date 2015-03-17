0b7f22df45a1082d517a4ca87e1a84de392a9c1d
Palani Eswaran <palani14@stanford.edu>
2015-03-17 14:08:43 -0700

    added logic in login post for reseting email

--- watchwithus/routes/login.js
+++ watchwithus/routes/login.js
@@ -9,7 +9,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-
+	var form_data = req.body;
+	db.resetPassword({
+	    email : form_data.email
+	  }, function(error) {
+	  if (error === null) {
+	    console.log("Password reset email sent successfully");
+	    res.render('login', { title: 'Login', 'passwordResetMessage': "Password reset email sent." });
+	  } else {
+	    console.log("Error sending password reset email:", error);
+	    res.render('reset', { title: 'Reset',  'passwordResetMessage': "Password reset email not sent. Please try again."});
+	  }
+	});
 });
 
 module.exports = router;
