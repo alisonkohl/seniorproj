a3fd8502eaa3fd695f25c8136d5c062aad489b6a
Alison Kohl <alison.kohl@gmail.com>
2015-03-17 14:51:31 -0700

    password reset implemented

--- watchwithus/routes/reset.js
+++ watchwithus/routes/reset.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Watch With Us' });
+	res.render('reset', {title: 'Watch With Us'});
 });
 
 module.exports = router;
