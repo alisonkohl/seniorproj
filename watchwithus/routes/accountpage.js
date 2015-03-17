20babea9dc81fe623ef6a94f4d14f0340256cebd
Alison Kohl <alison.kohl@gmail.com>
2015-03-17 13:06:36 -0700

    adding front end for changing emails, working on changing passwords

--- watchwithus/routes/accountpage.js
+++ watchwithus/routes/accountpage.js
@@ -6,26 +6,29 @@ var request = require('request');
 
 router.get('/', function(req, res, next){
 	var authData = db.getAuth();
+
+	if (authData == null) {
+		res.render('index', {title: 'Watch With Us'});
+	} else {
 	
-	uid = authData.uid;
+		uid = authData.uid;
 
-	var usersRef = new Firebase("https://watchwithus.firebaseio.com/users/" + uid);
+		var usersRef = new Firebase("https://watchwithus.firebaseio.com/users/" + uid);
 
 
-	db.orderByChild("users").on("child_added", function(snapshot) {
-		if (snapshot.key() == "users") {
-			var userIds = Object.keys(snapshot.val());
-			var arr = Object.keys(snapshot.val()).map(function(k) { return snapshot.val()[k] });
-			for (i = 0; i < arr.length; i++) {
-				if (userIds[i] == uid) {
-					var username = arr[i].username;
-					console.log(username);
-					//res.render('addfriends', {title: 'Account Page', 'username': username});
+		db.orderByChild("users").on("child_added", function(snapshot) {
+			if (snapshot.key() == "users") {
+				var userIds = Object.keys(snapshot.val());
+				var arr = Object.keys(snapshot.val()).map(function(k) { return snapshot.val()[k] });
+				for (i = 0; i < arr.length; i++) {
+					if (userIds[i] == uid) {
+						var username = arr[i].username;
+						res.render('account', {title: 'Account Page', 'username': username, 'email': authData.password.email });
+					}
 				}
 			}
-		}
-	});
-
+		});
+	}
 
 });
 
